b73912e1df604a91599a7f6787d771558a44d8ef
Mer Alvin Grita <mer.grita@stratusmeridian.com>
2020-04-14 15:11:26 +0800

    Add GNU General Public License V2 to the files.
    Add GNU General Public License V2 to the files.

--- src/Controller/AppsDashboardController.php
+++ src/Controller/AppsDashboardController.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * Stratus Meridian Apps Dashboard
+ * Copyright (C) 2020  Stratus Meridian LLC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 namespace Drupal\sm_appdashboard_apigee\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
@@ -9,9 +28,7 @@ use Drupal\Core\Url;
 
 /**
  * Defines AppsDashboardController class.
- *
  * Author: Mer Alvin A. Grita (mer.grita@stratusmeridian.com)
- *
  */
 class AppsDashboardController extends ControllerBase {
 	public function listApps() {
