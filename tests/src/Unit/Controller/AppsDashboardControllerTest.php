d0cb4d1b1f74e09a0e27cfe6da5c9896e1dafddc
Rakesh James <rakesh.gectcr@gmail.com>
2020-05-08 01:36:24 +0100

    DP-24: Added the skeleton with php unit WIP.

--- tests/src/Unit/Controller/AppsDashboardControllerTest.php
+++ tests/src/Unit/Controller/AppsDashboardControllerTest.php
@@ -29,8 +29,7 @@ class AppsDashboardControllerTest extends UnitTestCase {
 
     $prophecy = $this->prophesize('Drupal\sm_appdashboard_apigee\AppsDashboardStorage');
     $labels = $prophecy->labels()->willReturn(['labelDisplayName' => 'App Display Name', 'labelDisplayName1' => 'App Display Name1']);
-    print_r($labels);
-    exit;
+    $this->assertEmpty($labels);
 
   }
 }
