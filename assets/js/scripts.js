7adf35710fd4385640ed3e79212a9790da83e9d8
Mer Alvin Grita <meralvingrita24@gmail.com>
2020-05-21 10:19:37 +0800

    Update Module #10 New Features and code refactor 1. Add hook_help as required by coder module. 2. Code refactoring of js according to ESLint suggestion 3. Add readme.md

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,14 @@
-(function($) {
+(function($, Drupal, drupalSettings) {
 
-  if ($('form#appdetails-search-form').length) {
+	'use strict';
+
+  if($('form#appdetails-search-form').length) {
     var formSearch = $('form#appdetails-search-form');
     var datetimeContainer = formSearch.find('.sm-datetime-container');
     var detachLabel = datetimeContainer.find('h4.label').detach();
     var containerInline = datetimeContainer.find('.container-inline');
+
+  	$(detachLabel[0]).prependTo(containerInline[0]);
+  	$(detachLabel[1]).prependTo(containerInline[1]);
   }
-
-  $(detachLabel[0]).prependTo(containerInline[0]);
-  $(detachLabel[1]).prependTo(containerInline[1]);
-
 })(jQuery, Drupal, drupalSettings);
