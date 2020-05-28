77b59b7f2c100af9ba56b1fae3696f0da6ca0ff6
Rakesh James <rakesh.gectcr@gmail.com>
2020-05-28 20:02:27 +0100

    DP-24: Added the testListApps().

--- tests/src/Unit/Controller/AppsDashboardControllerTest.php
+++ tests/src/Unit/Controller/AppsDashboardControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\sm_appdashboard_apigee\Unit;
 
+use Drupal\sm_appdashboard_apigee\AppsDashboardStorageService;
 use Drupal\Tests\UnitTestCase;
 
 /**
@@ -9,22 +10,31 @@ use Drupal\Tests\UnitTestCase;
  */
 class AppsDashboardControllerTest extends UnitTestCase {
 
-  protected $prophecy;
-
+  /**
+   * The AppsDashboardStorageService under test.
+   *
+   * @var \Drupal\sm_appdashboard_apigee\AppsDashboardStorageService
+   */
+  protected $appsDashboardStorage;
 
   /**
    * {@inheritdoc}
    */
   protected function setUp() {
     parent::setUp();
-    $this->prophecy = $this->prophesize('Drupal\sm_appdashboard_apigee\AppsDashboardStorageService');
+    $this->appsDashboardStorage = $this->prophesize(AppsDashboardStorageService::class);
   }
 
   /**
    * Test the list apps.
    */
   public function testListApps() {
-    $labels = $this->prophecy->labels()->willReturn(['labelDisplayName' => 'App Display Name', 'labelDisplayName1' => 'App Display Name1']);
-    $this->assertNotEmpty($labels);
+    $labels = $this->appsDashboardStorage->labels()
+      ->willReturn([
+        'labelDisplayName' => 'App Display Name',
+        'labelDisplayName1' => 'App Display Name1'
+      ]);
+
+    $this->assertNotEmpty($labels, "Presence of Apps display name labels.");
   }
 }
